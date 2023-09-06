41075df3e85811e4d2f89a244a2c8c4fb29ff1cb
Matthew Renze <merenze@gmail.com>
2023-09-05 23:04:40 -0500

    Implement CommandTreeNode#execute

--- src/main/java/com/jellyrekt/commandregistrar/CommandExecutor.java
+++ src/main/java/com/jellyrekt/commandregistrar/CommandExecutor.java
@@ -1,5 +1,14 @@
 package com.jellyrekt.commandregistrar;
 
+import org.bukkit.command.CommandSender;
+
+import java.util.Map;
+
 public interface CommandExecutor {
-    // TODO
+    /**
+     * Execution logic for the command.
+     * @param sender The entity (player or console) sending the command.
+     * @param env The parameter names and argument values for this command.
+     */
+    public void execute(CommandSender sender, Map<String, String> env);
 }
