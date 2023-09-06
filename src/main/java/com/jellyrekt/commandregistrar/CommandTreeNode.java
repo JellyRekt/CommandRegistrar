68be6f3568fd29716eb4724a71fd838246a658a6
Matthew Renze <merenze@gmail.com>
2023-09-05 22:41:16 -0500

    CommandTreeNode construction

--- src/main/java/com/jellyrekt/commandregistrar/CommandTreeNode.java
+++ src/main/java/com/jellyrekt/commandregistrar/CommandTreeNode.java
@@ -1,5 +1,20 @@
 package com.jellyrekt.commandregistrar;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class CommandTreeNode {
-    // TODO
+    private Set<CommandTreeNode> children = new HashSet<>();
+    private Map<String, String> childAliases = new HashMap<>();
+    private String description;
+    private String usage;
+    private CommandExecutor commandExecutor;
+
+    CommandTreeNode(String description, String usage, CommandExecutor commandExecutor) {
+        this.description = description;
+        this.usage = usage;
+        this.commandExecutor = commandExecutor;
+    }
 }
